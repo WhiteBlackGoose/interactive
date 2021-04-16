97e7c89e4763d42a09244c6813669758fbc2e237
WhiteBlackGoose <wbg@angouri.org>
2021-04-16 18:42:47 +0300

    test added

---
+++ src/Microsoft.DotNet.Interactive.Jupyter.Tests/CancelTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.FSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.Jupyter.Tests
+{
+    public class CancelTests
+    {
+        [Fact] // #776
+        public async Task Cancel_infinite_loop_happens_within_3sec()
+        {
+             using var kernel = new FSharpKernel();
+
+             var code = 
+@"
+let mutable x = 0
+while true do
+    x <- x + 1
+";
+            var submitCode = new SubmitCode(code);
+
+            var task = kernel.SendAsync(submitCode);  // we don't want to await it. It's infinite, what we can do is only to interrupt
+
+            var interruption = kernel.SendAsync(new Cancel()); // cancelling
+
+            await Task.Delay(3000); // let's wait until something happens
+
+            task.IsCompleted.Should().BeTrue();
+        }
+    }
+}
